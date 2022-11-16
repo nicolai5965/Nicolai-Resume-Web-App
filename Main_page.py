0fc19e1878ec9e2c5bf8cd1a94b4654f17c34b24
Nicolai Søderberg <104358260+nicolai5965@users.noreply.github.com>
2022-11-16 16:54:32 +0100

    Update Main_page.py

--- Main_page.py
+++ Main_page.py
@@ -111,6 +111,10 @@ st.write(
 link = '[GitHub](http://github.com)'
 st.markdown(link, unsafe_allow_html=True)
 
+st.sidebar.markdown(f'''
+<a href={link}><button style="background-color:GreenYellow;">Github</button></a>
+''',
+unsafe_allow_html=True)
 if st.sidebar.button('LinkedIn'):
     webbrowser.open_new_tab(list(SOCIAL_MEDIA.values())[0])
 
