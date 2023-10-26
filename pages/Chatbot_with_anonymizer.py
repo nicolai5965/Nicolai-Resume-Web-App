13fb60c07eb6ba7c6149335ada35bf6f2d5af17c
Nicolai Søderberg <104358260+nicolai5965@users.noreply.github.com>
2023-10-26 10:34:53 +0200

    change

--- pages/Chatbot_with_anonymizer.py
+++ pages/Chatbot_with_anonymizer.py
@@ -347,9 +347,17 @@ use_faker = st.sidebar.checkbox("Use Faker", value=True)
 
 # Toggle for using custom patterns in the sidebar
 use_custom_patterns = st.sidebar.checkbox("Use Custom Patterns", value=True)
+if use_custom_patterns != st.session_state.get('prev_use_custom_patterns', True):
+    st.session_state.show_anonymizing = False
+    st.session_state.prev_use_custom_patterns = use_custom_patterns
+
 
 # Toggle for using custom faker operators in the sidebar
 use_custom_faker_operators = st.sidebar.checkbox("Use Custom Faker Operators", value=True)
+if use_custom_faker_operators != st.session_state.get('prev_use_custom_faker_operators', True):
+    st.session_state.show_anonymizing = False
+    st.session_state.prev_use_custom_faker_operators = use_custom_faker_operators
+
 
 
 # 2. Document Input
