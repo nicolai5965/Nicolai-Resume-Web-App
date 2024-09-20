daf8b4e7116f439bc046de7829276c7470149b0d
Nicolai Søderberg <104358260+nicolai5965@users.noreply.github.com>
2024-09-20 13:12:56 +0200

    Update GraphDB Conversational Explorer.py

--- pages/GraphDB Conversational Explorer.py
+++ pages/GraphDB Conversational Explorer.py
@@ -328,13 +328,20 @@ You have access to the following tools:
 {tools}
 
 To use a tool, please use the following format:
-Thought: Do I need to use a tool? Yes Action: the action to take, should be one of [{tool_names}] 
 
-Action Input: the input to the action 
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
 
-Observation: the result of the action when you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
 
-Thought: Do I need to use a tool? No Final Answer: [your response here]
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
 
 Begin!
 
