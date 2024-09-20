af43ef5008206f6b9c9472026207b2cc41f79bfd
Nicolai Søderberg <104358260+nicolai5965@users.noreply.github.com>
2024-09-20 12:06:20 +0200

    Update GraphDB Conversational Explorer.py

--- pages/GraphDB Conversational Explorer.py
+++ pages/GraphDB Conversational Explorer.py
@@ -224,8 +224,8 @@ def write_message(role, content, save=True):
     if save:
         st.session_state.messages.append({"role": role, "content": content})
 
-    with st.chat_message(role):
-        st.markdown(content)
+    # with st.chat_message(role):
+    #     st.markdown(content)
 
 def get_session_id():
     ctx = get_script_run_ctx()
